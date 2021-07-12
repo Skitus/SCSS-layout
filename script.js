c106d9e3ba4da5918b6f4a51d3a03d981a34031e
Skitus <artur.demenskiy03@gmail.com>
2021-07-12 09:04:35 +0300

    add files to git hub

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$('.slider').slick({
+			dots: true,
+			adaptiveHeight: true,
+			slidesToShow: 1
+	});
+});
+
+
+
+let plus = document.getElementsByClassName('plus');
+
+for (var i = 0; i < plus.length; i++) {
+	plus[i].addEventListener('click' , ShowAnser);
+}
+
+	function ShowAnser () {
+if (this.className === "plus") {
+	let med = this.parentNode.parentNode;
+	let res = med.getElementsByClassName('answer');
+
+	for (var q = 0; q < res.length; q++) {
+		res[q].style.display = 'block';
+
+	}
+	this.className +="active";
+}	else {
+		this.className = "plus";	
+			let med = this.parentNode.parentNode;
+	let res = med.getElementsByClassName('answer');
+		for (var q = 0; q < res.length; q++) {
+		res[q].style.display = 'none';
+
+	}
+	}
+}
+
+setTimeout(function () {
+    if(window.location.hash != '#r') {
+        window.location.hash = 'r';
+      window.location.reload(1);
+    }
+}, 5000);
